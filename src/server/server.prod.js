aed376c45e9643f9198c794d0ea5a58ae93a948f
adanjz <adanzweig@gmail.com>
2018-09-15 11:29:49 -0300

    fix logins + channels

--- src/server/server.prod.js
+++ src/server/server.prod.js
@@ -106,7 +106,7 @@ function renderFullPage(html, initialState) {
         <script>
           window.__INITIAL_STATE__ = ${JSON.stringify(initialState)}
         </script>
-        <script src="/dist/bundle.js"></script>
+        <script src="/bundle.js"></script>
       </body>
     </html>
   `
