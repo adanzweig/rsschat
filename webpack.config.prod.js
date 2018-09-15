070265e78f885314cc75df9a429cca7ac5596271
adanjz <adanzweig@gmail.com>
2018-09-15 12:27:59 -0300

    fix logins + channels

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: [
     'babel-polyfill',
     'webpack-hot-middleware/client',
-    './src/client/index'
+    '/app/src/client/index'
   ],
   output: {
     path: path.resolve(__dirname, './static/dist'),
